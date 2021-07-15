fc3adda79f25e621b1e0bdadb5a9915739b7f5c9
João <jvd.ramos@hotmail.com>
2021-07-15 18:30:06 -0300

    Adicionado carregamento de fontes da aplicação

--- App.tsx
+++ App.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
-import { SignIn } from './src/screens/Signin';
-export default function App(){
-  return(
-    <SignIn/>
-  )
+import React from "react";
+import { StatusBar } from "react-native";
+import { useFonts } from "expo-font";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
+import AppLoading from "expo-app-loading";
+import { Routes } from "./src/routes";
+
+export default function App() {
+  const [fontLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  if (!fontLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Routes />
+    </>
+  );
 }
