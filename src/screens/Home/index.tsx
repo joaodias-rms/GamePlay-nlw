8ccde5769e7514e8f836b60de615c640a6497dd2
João <jvd.ramos@hotmail.com>
2021-07-16 16:43:14 -0300

    Adicionado novos componetes na home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { View } from 'react-native';
 import {Profile} from '../../components/Profile'
+import { ButtonAdd } from '../../components/ButtonAdd';
 import { styles } from './styles';
+import {CategorySelect} from '../../components/CategorySelect';
 
 export function Home() {
   return (
     <View>
       <View style={styles.header}>
       <Profile/>
+      <ButtonAdd/>
       </View>
+    <View>
+      <CategorySelect/>
+    </View>
     </View>
   )
 }
