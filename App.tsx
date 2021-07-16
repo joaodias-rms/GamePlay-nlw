d0d5a99c783dba1407002790fb8b01ff7b74149d
João <jvd.ramos@hotmail.com>
2021-07-16 16:45:26 -0300

    Adicionado componente de background

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import {
 } from "@expo-google-fonts/rajdhani";
 import AppLoading from "expo-app-loading";
 import { Routes } from "./src/routes";
+import { Background } from "./src/components/Background";
 
 export default function App() {
   const [fontLoaded] = useFonts({
@@ -22,13 +23,13 @@ export default function App() {
   }
 
   return (
-    <>
+    <Background>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <Routes />
-    </>
+    </Background>
   );
 }
