952113640216453200c37e034a176b1d1210511d
João <jvd.ramos@hotmail.com>
2021-07-16 16:43:44 -0300

    Alterado componente de botão da tela inicial

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,23 +1,22 @@
 import React from "react";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 
-import {Text, Image, View, TouchableOpacity} from "react-native"
+import { Text, Image, View } from "react-native";
 
-import DiscordImg from '../../assets/discord.png'
-import {styles} from './styles'
+import DiscordImg from "../../assets/discord.png";
+import { styles } from "./styles";
 
-type Props = {
-  title: string,
-}
+type Props = RectButtonProps & {
+  title: string;
+};
 
-export function ButtonIcon({title}: Props){
+export function ButtonIcon({ title, ...rest }: Props) {
   return (
-    <TouchableOpacity style={styles.container} activeOpacity={0.7}>
+    <RectButton style={styles.container} {...rest}>
       <View style={styles.iconWrapper}>
-      <Image source={DiscordImg} style={styles.icon}/>
+        <Image source={DiscordImg} style={styles.icon} />
       </View>
-      <Text style={styles.title}>
-        {title}
-      </Text>
-    </TouchableOpacity>
+      <Text style={styles.title}>{title}</Text>
+    </RectButton>
   );
 }
