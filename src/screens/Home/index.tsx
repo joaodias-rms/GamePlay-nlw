bfbbb9934f24d4b35fa58acd5510f765420ef2be
João <jvd.ramos@hotmail.com>
2021-07-20 17:45:43 -0300

    Adicionado comp de categoria e lista de partidas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,60 @@
-import React from 'react';
-import { View } from 'react-native';
-import {Profile} from '../../components/Profile'
-import { ButtonAdd } from '../../components/ButtonAdd';
-import { styles } from './styles';
-import {CategorySelect} from '../../components/CategorySelect';
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+import { Profile } from "../../components/Profile";
+import { ButtonAdd } from "../../components/ButtonAdd";
+import { CategorySelect } from "../../components/CategorySelect";
+import { ListHeader } from "../../components/ListHeader";
 
+import { styles } from "./styles";
+import { FlatList } from "react-native-gesture-handler";
+import { Appointment } from "../../components/Appointment";
 export function Home() {
+  const [category, setCategory] = useState("");
+
+  const appointments = [
+    {
+      id: "1",
+      guild: {
+        id: "1",
+        name: "Lendários",
+        icon: null,
+        owner: true,
+      },
+      category: "1",
+      date: "22/06 às 20h40",
+      description:
+        "É hoje que vamos chegar ao challenger sem perder uma partida md10",
+    },
+  ];
+
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory("") : setCategory(categoryId);
+  }
+
   return (
     <View>
       <View style={styles.header}>
-      <Profile/>
-      <ButtonAdd/>
+        <Profile />
+        <ButtonAdd />
       </View>
-    <View>
-      <CategorySelect/>
+      <View>
+        <CategorySelect
+          categorySelected={category}
+          setCategory={handleCategorySelect}
+        />
+
+        <View style={styles.content}>
+          <ListHeader title="Partidas Agendadas" subtitle="Total 6" />
+
+          <FlatList
+            data={appointments}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item})=>(
+              <Appointment data={item}/>
+            )}
+          />
+        </View>
+      </View>
     </View>
-    </View>
-  )
+  );
 }
