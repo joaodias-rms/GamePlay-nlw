2eeebd932e49aeae092843d2279ba7458317017a
João <jvd.ramos@hotmail.com>
2021-07-20 17:43:58 -0300

    estilização de componentes

--- src/components/GuildIcon/styles.ts
+++ src/components/GuildIcon/styles.ts
@@ -1,7 +1,11 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container:{
-    minHeight: 120,
+  image:{
+    width: 64,
+    height: 64,
+    borderRadius: 8,
+    marginRight: 20
+    
   }
 })
