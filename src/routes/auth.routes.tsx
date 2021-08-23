e77053951bdfdcc93dd81a1f14d4d93b663586dd
joao <joao.victor@topsapp.com.br>
2021-08-23 17:28:48 -0400

    ajuste de rotas

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -3,27 +3,26 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import { Home } from "../screens/Home";
 import { SignIn } from "../screens/Signin";
+import { AppointmentDetails } from "../screens/AppointmentDetails";
+import { AppointmentCreate } from "../screens/AppointmentCreate";
+import { theme } from "../global/styles/theme";
 
 const { Navigator, Screen } = createStackNavigator();
 
 export function AuthRoutes() {
   return (
     <Navigator
-    headerMode="none"
-    screenOptions={{
-      cardStyle:{
-        backgroundColor: 'transparent'
-      }
-    }}
+      headerMode="none"
+      screenOptions={{
+        cardStyle: {
+          backgroundColor: theme.colors.secondary100,
+        },
+      }}
     >
-      <Screen 
-      name="SignIn" 
-      component={SignIn} 
-      />
-      <Screen 
-      name="Home" 
-      component={Home} 
-      />
+      <Screen name="SignIn" component={SignIn} />
+      <Screen name="Home" component={Home} />
+      <Screen name="AppointmentDetails" component={AppointmentDetails} />
+      <Screen name="AppointmentCreate" component={AppointmentCreate} />
     </Navigator>
   );
 }
