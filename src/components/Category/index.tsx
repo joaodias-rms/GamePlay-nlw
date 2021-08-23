c8869570901b56f6944d2a17ae89b69c3a3be106
joao <joao.victor@topsapp.com.br>
2021-08-23 17:27:10 -0400

    componente refatorado

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -9,25 +9,29 @@ import { styles } from "./styles";
 type Props = RectButtonProps & {
   title: string;
   icon: React.FC<SvgProps>;
+  hasCheckBox?: boolean;
   checked?: boolean;
 };
 export function Category({
   title,
   icon: Icon,
   checked = false,
+  hasCheckBox = true,
   ...rest
 }: Props) {
-  const { secondary50, secondary70 } = theme.colors;
+  const { secondary40, secondary50, secondary70, secondary85 } = theme.colors;
   return (
     <RectButton {...rest}>
       <LinearGradient
         style={styles.container}
         colors={[secondary50, secondary70]}
       >
-        <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
-          <View style={
+        <LinearGradient style={[styles.content, { opacity: checked ? 1 : 0.5 }]}
+        colors={[ checked ? secondary85 : secondary50, secondary40]}
+        >
+         { hasCheckBox && <View style={
             checked ? styles.checked : styles.check} 
-          />
+          />}
           <Icon 
           width={48} 
           height={48} 
@@ -35,7 +39,7 @@ export function Category({
           <Text style={styles.title}>
             {title}
           </Text>
-        </View>
+        </LinearGradient>
       </LinearGradient>
     </RectButton>
   );
