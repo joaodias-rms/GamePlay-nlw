51e607795f3c13325d001d2f18a9d44c371a8d7e
joao <joao.victor@topsapp.com.br>
2021-08-23 17:28:15 -0400

    criada tela de cadastro de partidas

---
+++ src/screens/AppointmentCreate/index.tsx
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+import { Text, View, } from "react-native";
+
+
+import { Background } from "../../components/Background";
+import { CategorySelect } from "../../components/CategorySelect";
+import { Header } from "../../components/Header";
+
+
+import { styles } from "./styles";
+import { theme } from "../../global/styles/theme";
+
+export function AppointmentCreate() {
+  const [category, setCategory] = useState("");
+
+  return (
+    <Background>
+      <Header
+        title="Agendar Partida"
+      />
+
+      <Text style={styles.label}>
+        Categoria
+      </Text>
+      
+      <CategorySelect hasCheckBox categorySelected={category} setCategory={setCategory}/>
+
+      <View style={styles.form}>
+
+      </View>
+    </Background>
+  );
+}
