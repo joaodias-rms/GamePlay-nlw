6fc2ea8789f53b34e2f77ad48cddb3ae3bcda1b6
joao <joao.victor@topsapp.com.br>
2021-08-23 17:44:31 -0400

    componente refatorado

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -1,11 +1,9 @@
-import React, {useState} from "react";
-import { Text, View, } from "react-native";
-
+import React, { useState } from "react";
+import { Text, View } from "react-native";
 
 import { Background } from "../../components/Background";
 import { CategorySelect } from "../../components/CategorySelect";
 import { Header } from "../../components/Header";
-
 
 import { styles } from "./styles";
 import { theme } from "../../global/styles/theme";
@@ -15,19 +13,17 @@ export function AppointmentCreate() {
 
   return (
     <Background>
-      <Header
-        title="Agendar Partida"
+      <Header title="Agendar Partida" />
+
+      <Text style={styles.label}>Categoria</Text>
+
+      <CategorySelect
+        hasCheckBox
+        categorySelected={category}
+        setCategory={setCategory}
       />
 
-      <Text style={styles.label}>
-        Categoria
-      </Text>
-      
-      <CategorySelect hasCheckBox categorySelected={category} setCategory={setCategory}/>
-
-      <View style={styles.form}>
-
-      </View>
+      <View style={styles.form}></View>
     </Background>
   );
 }
