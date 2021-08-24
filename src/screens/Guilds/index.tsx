87988fb4b0a0f238e2d07ddd5f160256497beb43
joao <joao.victor@topsapp.com.br>
2021-08-24 15:52:54 -0400

    Adicionado componente de listagem de guilds

---
+++ src/screens/Guilds/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { View, Text, FlatList } from "react-native";
+
+import { Guild, GuildProps } from "../../components/Guild";
+import { ListDivider } from "../../components/ListDivider";
+
+import { styles } from "./styles";
+
+type Props = {
+  handleGuildSelect: (guild: GuildProps) => void; 
+}
+
+export function Guilds({handleGuildSelect}: Props) {
+  const guilds = [
+    {
+      id: "1",
+      name: "Lendários",
+      icon: null,
+      owner: true,
+    },
+    {
+      id: "2",
+      name: "Lendários",
+      icon: null,
+      owner: true,
+    },
+  ];
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={guilds}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => 
+          <Guild 
+            data={item} 
+            onPress={()=> handleGuildSelect(item)}
+          />}
+        showsVerticalScrollIndicator={false}
+        ItemSeparatorComponent={() => <ListDivider />}
+        style={styles.guilds}
+      />
+    </View>
+  );
+}
