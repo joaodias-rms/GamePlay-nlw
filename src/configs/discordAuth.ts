a644daacda0fe93f5ff38129246acfacf17c9a10
joao <joao.victor@topsapp.com.br>
2021-08-26 11:29:32 -0400

    componentes removidos

--- src/configs/discordAuth.ts
+++
@@ -1,7 +0,0 @@
-const REDIRECT_URI = "https%3A%2F%2Fauth.expo.io%2F%40joaodias-rms%2Fgameplay";
-const SCOPE = "identify%20email%20connections%20guilds";
-const RESPONSE_TYPE = "token";
-const CLIENT_ID = "880138768852586496";
-const CDN_IMG = "https://cdn.discordapp.com";
-
-export { REDIRECT_URI, SCOPE, RESPONSE_TYPE, CLIENT_ID, CDN_IMG };
