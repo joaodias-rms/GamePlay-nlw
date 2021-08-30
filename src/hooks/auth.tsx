590a13598ebff8a3f55b8ebc277394d1b2e71931
joao <joao.victor@topsapp.com.br>
2021-08-30 13:21:32 -0400

    ajustado tipagem para função de logout

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -31,6 +31,7 @@ type AuthContextData = {
   user: User;
   loading: boolean;
   signIn: () => Promise<void>;
+  signOut: () => Promise<void>;
 };
 
 type AuthProviderProps = {
@@ -83,6 +84,11 @@ function AuthProvider({ children }: AuthProviderProps) {
     }
   }
 
+  async function signOut() {
+    setUser({} as User);
+    await AsyncStorage.removeItem(COLLECTION_USERS);
+  }
+
   async function loadUserStorageData() {
     const storage = await AsyncStorage.getItem(COLLECTION_USERS);
 
@@ -98,7 +104,7 @@ function AuthProvider({ children }: AuthProviderProps) {
     loadUserStorageData();
   }, []);
   return (
-    <AuthContext.Provider value={{ user, signIn, loading }}>
+    <AuthContext.Provider value={{ user, signIn, loading, signOut }}>
       {children}
     </AuthContext.Provider>
   );
